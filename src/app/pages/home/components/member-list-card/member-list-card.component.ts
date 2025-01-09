1a9181859b893d700d9736ff1f549dd4ba1b8719
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-09 19:13:06 -0300

    feat: initial structure

---
+++ src/app/pages/home/components/member-list-card/member-list-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './member-list-card.component.html',
+  styleUrl: './member-list-card.component.scss'
+})
+export class MemberListCardComponent {
+
+}
