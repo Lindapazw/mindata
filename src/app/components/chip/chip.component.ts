2bb0c008abaafe5cc8c6446e745068d9d65d668c
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-12 20:53:18 -0300

    feat: add chip component

--- src/app/components/chip/chip.component.ts
+++ src/app/components/chip/chip.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chip',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './chip.component.html',
-  styleUrl: './chip.component.scss'
+  styleUrl: './chip.component.scss',
 })
 export class ChipComponent {
-
+  @Input() icon: string = '';
+  @Input() bgColor: string = '';
 }
