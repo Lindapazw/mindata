324b56864de3d21b08844ee9b7d2b8e99376eddf
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-12 12:15:13 -0300

    feat: add avatar

--- src/app/pages/home/components/member-list-card/member-list-card.component.ts
+++ src/app/pages/home/components/member-list-card/member-list-card.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { AvatarComponent } from '../../../../components/avatar/avatar.component';
+import { ButtonComponent } from '../../../../components/button/button.component';
 
 @Component({
   selector: 'app-member-list-card',
   standalone: true,
-  imports: [],
+  imports: [AvatarComponent, ButtonComponent],
   templateUrl: './member-list-card.component.html',
-  styleUrl: './member-list-card.component.scss'
+  styleUrl: './member-list-card.component.scss',
 })
-export class MemberListCardComponent {
-
-}
+export class MemberListCardComponent {}
