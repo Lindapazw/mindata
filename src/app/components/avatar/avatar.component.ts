a6b54ad2af9fa26bb625cc6f98dcaa9acb732cb7
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-12 19:00:39 -0300

    feat: add avatar component

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
   imports: [],
   templateUrl: './avatar.component.html',
-  styleUrl: './avatar.component.scss'
+  styleUrl: './avatar.component.scss',
 })
 export class AvatarComponent {
-
+  @Input() image?: string;
+  defaultImage: string = 'assets/images/no-user.png';
 }
