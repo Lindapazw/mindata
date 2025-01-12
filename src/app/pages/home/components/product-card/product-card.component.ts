c5e1b4f606548c687e6e93ff9223a218ac275144
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-12 20:53:01 -0300

    feat: add product card

--- src/app/pages/home/components/product-card/product-card.component.ts
+++ src/app/pages/home/components/product-card/product-card.component.ts
@@ -1,12 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../components/button/button.component';
+import { ChipComponent } from '../../../../components/chip/chip.component';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [ChipComponent, NgFor, ButtonComponent],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  chipsData = [
+    { icon: 'fa-solid fa-leaf', bgColor: '$green' },
+    { icon: 'fa-solid fa-fire', bgColor: '#f8593b' },
+  ];
 }
