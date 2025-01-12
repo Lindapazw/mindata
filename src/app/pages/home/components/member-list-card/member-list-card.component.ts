27b2e479687923cae97551731b1ed82654f54f61
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-12 19:18:42 -0300

    feat: add member list card

--- src/app/pages/home/components/member-list-card/member-list-card.component.ts
+++ src/app/pages/home/components/member-list-card/member-list-card.component.ts
@@ -1,12 +1,46 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { AvatarComponent } from '../../../../components/avatar/avatar.component';
-import { ButtonComponent } from '../../../../components/button/button.component';
+import { User } from '../../../../models/user.model';
+import { UserSummaryComponent } from './components/user-summary/user-summary.component';
 
 @Component({
   selector: 'app-member-list-card',
   standalone: true,
-  imports: [AvatarComponent, ButtonComponent],
+  imports: [UserSummaryComponent, NgFor],
   templateUrl: './member-list-card.component.html',
   styleUrl: './member-list-card.component.scss',
 })
-export class MemberListCardComponent {}
+export class MemberListCardComponent {
+  members: User[] = [
+    {
+      id: 1,
+      name: 'Wade Warren',
+      occupation: 'Dog Trainer',
+      image: 'assets/images/avatar-1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Robert Fox',
+      occupation: 'President of Sales',
+      image: 'assets/images/avatar-2.jpg',
+    },
+    {
+      id: 3,
+      name: 'Jane Cooper',
+      occupation: 'Nursing Assistant',
+      image: 'assets/images/avatar-3.jpg',
+    },
+    {
+      id: 4,
+      name: 'Frank Esteban',
+      occupation: 'Software Tester',
+      image: 'assets/images/avatar-4.jpg',
+    },
+    {
+      id: 5,
+      name: 'Dianne Russell',
+      occupation: 'Web Designer',
+      image: 'assets/images/avatar-5.jpg',
+    },
+  ];
+}
