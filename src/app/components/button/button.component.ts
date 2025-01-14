953bacf74dbd31d898ddf3178b6a750a0343f23c
Linda Paz <Lindacsndelapaz@gmail.com>
2025-01-13 23:41:45 -0300

    feat: add button variant

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-
+  @Input() type: 'variant1' | 'variant2' | 'variant3' | 'variant4' = 'variant1';
+  @Input() label: string = 'Follow';
 }
